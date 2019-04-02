9362a8c3926e673de1eb74924d32f1f6833fa64c
phantasyOne <nexus4@my.com>
2019-04-02 12:29:31 +0300

    laba 1 Done

--- Mathphysics/laba1.py
+++ Mathphysics/laba1.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 l = 1
-n = 40
+n = 100
 h = l/n
 
 
@@ -11,18 +11,18 @@ g = lambda x: 1
 p = lambda x: 1
 f = lambda x: -x
 
-# fi = lambda x, i: x * sin(cos(x)**i) #24
-# fi = lambda x, i: x**i * exp(i*x) #9
-# fi = lambda x, i: x**i * (sin(x)**i + cos(x)**i) #10
-# fi = lambda x, i: x/(1 + x)**i #15 WIn
 def fi(x, i, x_index):
     if i == x_index:
         value = x * sin(cos(x)**i)
     else:
         value = 0
     return value
+# fi = lambda x, i: x**i * exp(i*x) #9
+# fi = lambda x, i, x_index: x**i * (sin(x)**i + cos(x)**i) #10
+# fi = lambda x, i, x_index: x/(1 + x)**i #15 WIn
+fi = lambda x, i, x_index: x * sin(cos(x)**i) #24
 
-# dfi = lambda x, i: sin(cos(x)**i) - x*i*sin(x)*cos(x)**(i-1)*cos(x)**i
+
 def dfi(x, i, x_index):
     if i == x_index:
         value = sin(cos(x)**i) - x*i*sin(x)*cos(x)**(i-1)*cos(x)**i
@@ -30,8 +30,11 @@ def dfi(x, i, x_index):
         value = 0
     return value
 
+dfi = lambda x, i, x_index: sin(cos(x)**i) - x*i*sin(x)*cos(x)**(i-1)*cos(cos(x)**i)
+# dfi = lambda x, i, x_index: x*1
+# dfi = lambda x, i, x_index: i*x**(i-1) * (sin(x)**i + cos(x)**i - x* sin(x)*cos(x)**(i-1) + x*cos(x)*sin(x)**(i-1))
 
-exact = lambda x: exp(1)*exp(x)/(exp(2) + 1) + exp(1)*exp(-x)/(exp(2) + 1) - x
+exact = lambda x: exp(1)*exp(x)/(exp(2) + 1) - exp(1)*exp(-x)/(exp(2) + 1) - x
 
 left_boundary = 0
 rigth_boundary = lambda U: U[-2]
@@ -49,9 +52,9 @@ X = np.linspace(0, l, n)
 
 A = []
 F = []
-for i in range(n):
+for i in range(1, n):
     ai = []
-    for j in range(n):
+    for j in range(1, n):
         firts_int = itegrate(lambda x, i, j, x_index: p(x) * dfi(x, j, x_index) * dfi(x, i, x_index), h, X, i, j)
         sec_int = itegrate(lambda x, i, j, x_index: g(x) * fi(x, j, x_index) * fi(x, i, x_index), h, X, i, j)
         aj = firts_int + sec_int
@@ -64,8 +67,9 @@ A = np.array(A)
 F = np.array(F)
 
 C = np.linalg.solve(A, F)
+print(len(range(1, n)))
 
-U = [sum([C[j] * fi(x, j, x_index) for j in range(n)]) + 0.58  for x_index, x in enumerate(X)]
+U = [sum([C[j-1] * fi(x, j, x_index) for j in range(1, n)]) for x_index, x in enumerate(X)]
 
 exact_U = np.array([exact(x) for x in X])
     
